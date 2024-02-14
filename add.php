860900a05a06bc620ae846ca33166aa1873b6de5
mehrabkb <mkordbacheh75@gmail.com>
2024-02-14 16:14:49 +0330

    i work on my code to clean code but comments not complete

--- add.php
+++ add.php
@@ -12,27 +12,9 @@
                 <label for="name">دسته بندی را انتخاب کنید</label>
                 <select name="products" id="">
                     <?php
-                        $categories = get_terms([
-                            'taxonomy' => 'product_cat',
-                            'hide_empty' => true,
-                            'parent' => 0
-                        ]);
+                        $categories = allCategories();
                         foreach ($categories as $key => $value) {
-                            $args = array(
-                                'post_type' => 'product',
-                                'numberposts' => -1,
-                                'post_status' => 'publish',
-                                'fields' => 'ids',
-                                'tax_query' => array(
-                                    array(
-                                        'taxonomy' => 'product_cat',
-                                        'field' => 'term_id',
-                                        'terms' => $value->term_id,
-                                        'operator' => 'IN',
-                                    ),
-                                ),
-                            );
-                            $all_ids = get_posts($args);
+                            $all_ids = getAllProductsIdBySelectingCategoryId($value->term_id);
                             $all_ids_count = count($all_ids);
                             echo "<option value='{$value->term_id}'> {$value->name}  {$all_ids_count} </option>" ;
                         }
@@ -41,7 +23,10 @@
                 <input type="submit" class="button button-primary" name="submit_form" value="ثبت">
             </form>
             </div>
+
         <?php
+        $finalTblShowingRecords = tableDataRender();
+        tableShow($finalTblShowingRecords);
     if (isset($_POST['submit_form'])){
         $categoy_id = $_POST['products'];
         $result = $wpdb->get_results("SELECT * FROM `wp_wtcplugin` WHERE `category_id` = $categoy_id");
@@ -54,18 +39,22 @@
             $wpdb->insert($table_name , $data , $format);
             $record_id = $wpdb->insert_id;
         }
-        $allTableRecords = $wpdb->get_results("SELECT * FROM `wp_wtcplugin`");
-        $finalTblShowingRecords = [];
-        foreach ($allTableRecords as $atr){
-            $category = get_term($atr->category_id);
-            $category_name = $category->name;
-            array_push($finalTblShowingRecords , [
-                    'id' => $atr->category_id,
-                    'category_name' => $category_name,
-            ]);
-        }
-
-
+        $finalTblShowingRecords = tableDataRender();
+        tableShow($finalTblShowingRecords);
+        ?>
+    <?php
+    }
+}
+    //this function get all products categories :)
+    function allCategories(){
+        $categories = get_terms([
+            'taxonomy' => 'product_cat',
+            'hide_empty' => true,
+            'parent' => 0
+        ]);
+        return $categories;
+    }
+    function getAllProductsIdBySelectingCategoryId($category_id){
         $args = array(
             'post_type' => 'product',
             'numberposts' => -1,
@@ -75,13 +64,33 @@
                 array(
                     'taxonomy' => 'product_cat',
                     'field' => 'term_id',
-                    'terms' => $categoy_id,
+                    'terms' => $category_id,
                     'operator' => 'IN',
                 ),
             ),
         );
-        $all_ids = get_posts($args);    ?>
-        
+        $all_ids = get_posts($args);
+        return $all_ids;
+    }
+    function tableDataRender(){
+        global $wpdb;
+        $allTableRecords = $wpdb->get_results("SELECT * FROM `wp_wtcplugin`");
+        $finalTblShowingRecords = [];
+        foreach ($allTableRecords as $atr){
+            $category = get_term($atr->category_id);
+            $category_name = $category->name;
+            array_push($finalTblShowingRecords , [
+                'id' => $atr->category_id,
+                'category_name' => $category_name,
+            ]);
+        }
+        return $finalTblShowingRecords;
+    }
+    function tableShow($finalTblShowingRecords){
+        ?>
+            <script>
+                document.querySelector('table#finalTbl').remove();
+            </script>
         <table id = 'finalTbl' class='display' style='width : 80%; margin : 0 auto;'>
             <thead>
                 <tr>
@@ -107,38 +116,9 @@
                     <td> <?php echo "[wtcTable cat_id=\"" .$finalTblShowingRecords[$i]['id'] . "\"]";  ?></td>
                   </tr>
                       <?php
-
                 }
-
-
-
             ?>
-
-
-
-<!--                --><?php
-//                $count = 0 ;
-//            for($i = 0 ; $i < count($all_ids) ; $i++){
-//                $product = wc_get_product($all_ids[$i]);
-//                ?>
-<!--            <tr>-->
-<!--                <td>--><?php //echo ++$count; ?><!--</td>-->
-<!--                <td>--><?php //echo $product->get_name(); ?><!--</td>-->
-<!--                <td>--><?php //echo number_format($product->get_price()); ?><!--</td>-->
-<!--            </tr>-->
-<!--        --><?php //
-//        }
-//        ?>
         </tbody>
         </table>
-    <?php
-    add_after_submit_form();
-    }   
-}
-function jsTblCaller(){
-    wp_register_script('myScript' , plugins_url('woocommerceTblPlugin/assets/script.js'));
-    wp_enqueue_script('myScript');
-}
-function add_after_submit_form(){
-    add_action('admin_print_scripts' , 'jsTblCaller');
-}   
+<?php
+    }
