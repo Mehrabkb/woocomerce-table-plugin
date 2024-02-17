412876cc4186c7b3c1866f58680ac3d8c07f573a
mehrabkb <mkordbacheh75@gmail.com>
2024-02-17 09:35:50 +0330

    add to cart button worked

--- woocommerceTblPlugin.php
+++ woocommerceTblPlugin.php
@@ -82,7 +82,7 @@ function wtc_ShortCode($atts){
                         <td class='unit-price' data-pure-price='".$product->get_price()."'> ". number_format($product->get_price()) ." </td>
                         <td> <button type='button' class='button' onclick='changInputCount(this)' data-role='decrease'>-</button> <input style='width: 40px;' id='count' type='number' value='0' min='0' max='100' disabled> <button type='button' class='button' onclick='changInputCount(this)' data-role='increase' class='increase-button' >+</button> </td>
                         <td class='final-price'>".number_format(0)."</td>
-                        <td><button class='button button-primary add_to_card_ajax_button'>افزودن</button></td>
+                        <td>" . add_to_cart_button($all_ids[$i]) . "</td>
                     </tr>";
     }
     $outPutHtml = "<table id='shortcode-tbl' class=\"cell-border display nowrap \" style=\" width: 100%; \">
@@ -114,10 +114,51 @@ function wtc_ShortCode($atts){
 
 }
 
+function add_to_cart_button($product_id) {
+    // Output your button HTML here
+    return '<button class="add-to-cart-btn" data-product-id="'. $product_id .'">افزودن به سبد خرید</button>';
+}
+add_action('woocommerce_after_single_product_summary', 'add_to_cart_button');
+add_action('wp_ajax_custom_add_to_cart', 'custom_add_to_cart');
+add_action('wp_ajax_nopriv_custom_add_to_cart', 'custom_add_to_cart');
+function custom_add_to_cart() {
+    global $woocommerce;
+    $product_id = $_POST['product_id'];
+    $quantity = isset($_POST['quantity']) ? intval($_POST['quantity']) : 1;
+
+    $result = WC()->cart->add_to_cart($product_id, $quantity);
+    if($result){
+        return $woocommerce;
+    }
+    die();
+}
 add_shortcode("wtcTable" , "wtc_ShortCode");
-add_action('wp_ajax_my_plugin_action', 'my_plugin_ajax_handler');
+add_action('wp_footer', 'my_plugin_ajax_handler');
 function my_plugin_ajax_handler() {
-    // Handle AJAX request here
+    // Handle AJAX request here?>
+<script type="text/javascript">
+    jQuery(document).ready(function($){
+        console.log('hello');
+        $('.add-to-cart-btn').on('click' , function(e){
+            e.preventDefault();
+            let that = $(this);
+            let url = that.parent().parent().attr('data-url-ajax');
+            let product_id = that.parent().parent().attr('data-product-id');
+            let inputCount = parseInt(that.parent().parent().find('input#count').val()) ;
+            var data = {
+                'action': 'custom_add_to_cart',
+                'product_id': product_id,
+                'quantity' : inputCount
+            };
+            $.post('<?php echo admin_url('admin-ajax.php'); ?>', data, function(response) {
+                // Optionally, you can handle the response here
+                console.log(response);
+                alert('با موفقیت به سبد خرید اضافه شد');
+            });
+    });
+    });
+</script>
+<?php
     // You can access POST data using $_POST array
     // Return the response using echo or wp_send_json()
     wp_send_json_success('Success!');
